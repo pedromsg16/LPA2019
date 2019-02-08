89da4ce2162f98c70bd056c9d1393b34b8cdfec4
pedromsg16 <pedromsg16@gmail.com>
2019-02-08 11:41:47 +0000

    Add files via upload
    Exercise 2 already accepted on mooshak

---
+++ example2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+string exercise2(string str) {
+    stack<char> mystack;
+    char elem;
+
+    for (unsigned i = 0; i < str.length(); i++){
+        if (mystack.empty()) {
+            if (str[i] == '(' or str[i] == '['){
+                mystack.push(str[i]);
+            } else {
+                return "No\n";
+            }
+        }
+        else{
+            elem = mystack.top();
+            if (str[i] == ')' && elem == '('){
+                mystack.pop();
+            } else if (str[i] == ']' && elem == '['){
+                mystack.pop();
+            } else if (str[i] == '(' || str[i] == '['){
+                mystack.push(str[i]);
+            } else {
+            	return "No\n";
+            }
+        }
+    }
+
+    if (!mystack.empty()) {
+        return "No\n";
+    }
+    return "Yes\n";
+}
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    string sequence;
+    string answer;
+    
+    cin >> num;
+
+    for (int i = 0; i < num; i++) {
+        cin >> sequence;
+
+        answer += exercise2(sequence);
+        
+    }
+
+    cout << answer;
+
+    return 0;
+}
